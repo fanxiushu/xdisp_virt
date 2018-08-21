97b58fd2adf40ec2ed5f35e956f495c7759d20a7
Fanxiushu <fanxiushu@163.com>
2018-08-21 13:36:35 +0800

    xfsredir files system uploaded

---
+++ html/jsvpx/jsvpx_worker.js
@@ -0,0 +1,29 @@
+if (typeof (JsVpx) === "undefined") {
+    importScripts('jsvpx.js')
+}
+///
+function onMessage(e) {
+    var message = e.data;
+    switch (message.type) {
+        case 'queueInput':
+            var img = decoder.decode(message.data);
+            if (!img) break;
+            var output = new Uint8Array(img.img_data);
+            var crop = { left: 20, top: 10, width: img.d_w + 20, height: img.d_h + 10 };
+
+            postMessage({
+                'type': 'pictureReady',
+                'width': img.w,
+                'height': img.h,
+                'croppingParams': crop,
+                'data': output.buffer,
+            }, [output.buffer]);
+
+            break;
+    }
+}
+
+////
+addEventListener('message', onMessage);
+var decoder = new JsVpx();
+

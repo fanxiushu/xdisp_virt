244c33ab49e62bcb9d396aa1c2eee54a5cf42446
Fanxiushu <fanxiushu@163.com>
2024-02-26 22:42:32 +0800

    xfsredir files system uploaded

---
+++ indirect_display/driver/install_native.bat
@@ -0,0 +1,19 @@
+@ echo off
+%1 %2
+ver|find "5.">nul&&goto :Admin
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin %cd%","","runas",1)(window.close)&goto :eof
+:Admin
+
+set ARCHITECTURE=%PROCESSOR_ARCHITECTURE%
+if "%ARCHITECTURE%"=="AMD64" (
+     cd /d %~dp0\x64
+) else if "%ARCHITECTURE%"=="x86" (
+     cd /d %~dp0\x86
+) else (
+     cd /d %~dp0
+)
+
+
+rundll32.exe indirect_display.dll,rundll32_execute install_native
+
+pause

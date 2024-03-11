edabb247f7057a9b79988e29911326f360ecea7b
Fanxiushu <fanxiushu@163.com>
2024-03-11 11:36:13 +0800

    xfsredir files system uploaded

---
+++ indirect_display/driver/start_swd.bat
@@ -0,0 +1,7 @@
+@ echo off
+%1 %2
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin %cd%","","runas",1)(window.close)&goto :eof
+:Admin
+
+
+rundll32.exe %SystemRoot%\system32\drivers\UMDF\indirect_display.dll,rundll32_execute start_swd
